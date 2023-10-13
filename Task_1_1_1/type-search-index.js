22394dbc84af874cd39966a0900402fcb27f0751
Routybor <Routybor@users.noreply.github.com>
2023-10-13 12:55:38 +0000

    Deploying to gh-pages from @ Routybor/OOP@03f64fde962b1a6b9055fe2c112e07483f856b7b 🚀

--- Task_1_1_1/type-search-index.js
+++ Task_1_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"solution"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Heapsort"}]
