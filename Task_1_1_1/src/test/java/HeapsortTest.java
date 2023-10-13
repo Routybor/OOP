764306d1b35c0cde8d6f160c403a3f9145fb95f0
Routybor <routybor@gmail.com>
2023-10-13 19:54:48 +0700

    Task_1_1_1 fixes 2

--- Task_1_1_1/src/test/java/HeapsortTest.java
+++ Task_1_1_1/src/test/java/HeapsortTest.java
@@ -3,25 +3,25 @@ import org.junit.jupiter.api.Test;
 class HeapsortTest {
     @Test
     void heapsort_one_elem_test() {
-        int[] ans = solution.heapsort(new int[] {1});
+        int[] ans = Heapsort.heapsort(new int[] {1});
         Assertions.assertArrayEquals(new int[] {1}, ans);
     }
 
     @Test
     void heapsort_empty_test() {
         Assertions.assertArrayEquals(new int[] {},
-                solution.heapsort(new int[] {}));
+                Heapsort.heapsort(new int[] {}));
     }
 
     @Test
     void heapsort_test1() {
-        int[] ans = solution.heapsort(new int[] {1234, 5, 5, 222, 131311, 0, 1});
+        int[] ans = Heapsort.heapsort(new int[] {1234, 5, 5, 222, 131311, 0, 1});
         Assertions.assertArrayEquals(new int[] {0, 1, 5, 5, 222, 1234, 131311}, ans);
     }
 
     @Test
     void heapsort_test2() {
-        int[] ans = solution.heapsort(new int[] {0, 148, -137, -137, 2, 23});
+        int[] ans = Heapsort.heapsort(new int[] {0, 148, -137, -137, 2, 23});
         Assertions.assertArrayEquals(new int[] {-137, -137, 0, 2, 23, 148}, ans);
     }
 }
