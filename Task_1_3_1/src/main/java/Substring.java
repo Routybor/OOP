baa0efe90951394ea6cf0265a6336eb944a7cdc7
Routybor <routybor@gmail.com>
2023-11-17 14:30:15 +0700

    Task_1_3_1 solution

--- Task_1_3_1/src/main/java/Substring.java
+++ Task_1_3_1/src/main/java/Substring.java
@@ -3,6 +3,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -23,12 +24,13 @@ public class Substring {
         List<Integer> indexes = new ArrayList<>();
         // для поддержки UTF-8, из строки получается массив байтов
         byte[] substringBytes = substring.getBytes(StandardCharsets.UTF_8);
-        byte[] buffer = new byte[1024];
+        byte[] buffer = new byte[10];
 
         // открывается поток для чтения файла
         try (var inputStream = Files.newInputStream(Path.of(fileName))) {
             int bytesRead;
             int indexesSize = 0;
+            int totalBytesRead = 0;
 
             // файл считывается по частям в буфер
             while ((bytesRead = inputStream.read(buffer)) != -1) {
@@ -40,7 +42,7 @@ public class Substring {
                         // найдено совпадение => продолжается проверка следующих байтов подстроки
                         if (indexesSize == substringBytes.length) {
                             // Найдено полное вхождение подстроки, сохраняется позицию
-                            indexes.add(i - indexesSize + 1);
+                            indexes.add(totalBytesRead + i - indexesSize + 1);
                             indexesSize = 0;
                         }
                     } else {
@@ -48,6 +50,7 @@ public class Substring {
                         indexesSize = 0;
                     }
                 }
+                totalBytesRead += bytesRead;
             }
         }
 
@@ -61,8 +64,8 @@ public class Substring {
      */
     public static void main(String[] args) {
         try {
-            List<Integer> result = find("src/main/resources/input.txt", "DDD");
-            System.out.println("DDD = " + result);
+            List<Integer> result = find("src/main/resources/input.txt", "try");
+            System.out.println("try = " + result);
 
             result = find("src/main/resources/input.txt", "хаха");
             System.out.println("xaxa = " + result);
--- Task_1_3_1/src/test/java/UnitTests.java
+++ Task_1_3_1/src/test/java/UnitTests.java
@@ -1,2 +1,70 @@
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Юнит тесты.
+ */
 public class UnitTests {
+    @Test
+    void test1() {
+        // Можно проверить самостоятельно
+        ArrayList<Integer> realAnswer = new ArrayList<> (
+            Arrays.asList(0, 24, 62, 86)
+        );
+
+        try {
+            ArrayList<Integer> answer = (ArrayList<Integer>)Substring.find(
+                "src/test/resources/test1.txt",
+                "tryapochka"
+            );
+            Assertions.assertEquals(realAnswer, answer);
+        } catch (Exception ex) {
+            fail();
+        }
+    }
+
+    @Test
+    void test2() {
+        // Файл генерировался случайно, в строку было вставлено ровно 20 искомых подстрок,
+        // первое вхождение фиксированно и равно 50167 код выдаёт ровно 20 результатов
+        //  и первое вхождение совпадает, остальные позиции должны совпадать также
+        ArrayList<Integer> realAnswer = new ArrayList<> (
+            Arrays.asList(
+                50167,
+                83535,
+                89173,
+                98033,
+                125762,
+                127065,
+                134338,
+                205825,
+                231022,
+                246787,
+                273330,
+                275783,
+                298414,
+                340605,
+                361834,
+                413770,
+                416938,
+                444749,
+                459704,
+                476995
+            )
+        );
+
+        try {
+            ArrayList<Integer> answer = (ArrayList<Integer>) Substring.find(
+                "src/test/resources/test2.txt",
+                "tryapochka"
+            );
+            Assertions.assertEquals(realAnswer, answer);
+        } catch (Exception ex) {
+            fail();
+        }
+    }
 }
