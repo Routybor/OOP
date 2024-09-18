f378cf4f32c74692f8e966457475715be559d317
Daniel <routybor@gmail.com>
2024-09-18 19:04:50 +0700

    Added more rounds in unit test

--- Task_1_1_2/src/test/java/ru/nsu/yakhimovich/BlackjackUnitTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/yakhimovich/BlackjackUnitTest.java
@@ -1,8 +1,8 @@
 package ru.nsu.yakhimovich;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
 
 /**
  * Небольшой тест для проверки на функционирование кода.
@@ -12,12 +12,18 @@ public class BlackjackUnitTest {
     void mainTest() {
         // Эмуляция ввода из консоли
         String input = "1\n0\n";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
 
         // Ход игры
         int[] score = new int[2];
-        Blackjack game = new Blackjack();
-        game.startRound(1, score);
+        for (int i = 0; i < 10; i++) {
+            // Переустановить входную строку для программы
+            in.reset();
+
+            Blackjack game = new Blackjack();
+            game.startRound(1, score);
+        }
     }
 }
+
