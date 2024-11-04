56187b33b8a30009fc304070770469682b705f04
Daniel <routybor@gmail.com>
2024-11-04 17:19:28 +0700

    Code style fixes 2

--- Task_1_2_1/src/test/java/ru/nsu/yakhimovich/graph/AbstractGraphTest.java
+++ Task_1_2_1/src/test/java/ru/nsu/yakhimovich/graph/AbstractGraphTest.java
@@ -1,12 +1,11 @@
 package ru.nsu.yakhimovich.graph;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 /**
  * Абстрактные юнит-тесты для графа.
