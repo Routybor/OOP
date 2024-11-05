fbfcc7d340566f2fbb52f2dabb1b7ce78dfed17d
Daniel <routybor@gmail.com>
2024-11-05 23:09:43 +0700

    Code fixes 2

--- Task_1_2_2/src/test/java/ru/nsu/yakhimovich/hashtable/HashTableUnitTest.java
+++ Task_1_2_2/src/test/java/ru/nsu/yakhimovich/hashtable/HashTableUnitTest.java
@@ -4,8 +4,7 @@ import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
 
 /**
  * Юнит тесты для хэш-таблиц.
@@ -23,17 +22,17 @@ class HashTableUnitTest {
         hashTable.put("HAHA", 1);
         hashTable.put("HOH", 2);
         hashTable.put("BLOB", 3);
-        assertEquals(1, hashTable.get("HAHA"));
-        assertEquals(2, hashTable.get("HOH"));
-        assertEquals(3, hashTable.get("BLOB"));
-        assertNull(hashTable.get("nananan"));
+        Assertions.assertEquals(1, hashTable.get("HAHA"));
+        Assertions.assertEquals(2, hashTable.get("HOH"));
+        Assertions.assertEquals(3, hashTable.get("BLOB"));
+        Assertions.assertNull(hashTable.get("nananan"));
     }
 
     @Test
     void testUpdate() {
         hashTable.put("HAHA", 1);
         hashTable.update("HAHA", 1.1);
-        assertEquals(1.1, hashTable.get("HAHA"));
+        Assertions.assertEquals(1.1, hashTable.get("HAHA"));
     }
 
     @Test
@@ -41,15 +40,15 @@ class HashTableUnitTest {
         hashTable.put("HAHA", 1);
         hashTable.put("HOH", 2);
         hashTable.remove("HAHA");
-        assertNull(hashTable.get("HAHA"));
-        assertEquals(2, hashTable.get("HOH"));
+        Assertions.assertNull(hashTable.get("HAHA"));
+        Assertions.assertEquals(2, hashTable.get("HOH"));
     }
 
     @Test
     void testContainsKey() {
         hashTable.put("HAHA", 1);
-        assertTrue(hashTable.containsKey("HAHA"));
-        assertFalse(hashTable.containsKey("HOH"));
+        Assertions.assertTrue(hashTable.containsKey("HAHA"));
+        Assertions.assertFalse(hashTable.containsKey("HOH"));
     }
 
     @Test
@@ -58,7 +57,7 @@ class HashTableUnitTest {
             hashTable.put("key" + i, i);
         }
         for (int i = 0; i < 20; i++) {
-            assertEquals(i, hashTable.get("key" + i));
+            Assertions.assertEquals(i, hashTable.get("key" + i));
         }
     }
 
@@ -69,9 +68,9 @@ class HashTableUnitTest {
         hashTable.put("HOH", 2);
         difTable.put("HAHA", 1);
         difTable.put("HOH", 2);
-        assertTrue(hashTable.equals(difTable));
+        Assertions.assertTrue(hashTable.equals(difTable));
         difTable.put("BLOB", 3);
-        assertFalse(hashTable.equals(difTable));
+        Assertions.assertFalse(hashTable.equals(difTable));
     }
 
     @Test
@@ -79,8 +78,8 @@ class HashTableUnitTest {
         hashTable.put("HAHA", 1);
         hashTable.put("HOH", 2);
         String output = hashTable.toString();
-        assertTrue(output.contains("HAHA=1"));
-        assertTrue(output.contains("HOH=2"));
+        Assertions.assertTrue(output.contains("HAHA=1"));
+        Assertions.assertTrue(output.contains("HOH=2"));
     }
 
     @Test
@@ -89,7 +88,7 @@ class HashTableUnitTest {
         hashTable.put("HOH", 2);
         Iterator<HashTable.Entry<String, Number>> iterator = hashTable.iterator();
         hashTable.put("BLOB", 3);
-        assertThrows(ConcurrentModificationException.class, iterator::next);
+        Assertions.assertThrows(ConcurrentModificationException.class, iterator::next);
     }
 
     @Test
@@ -99,18 +98,18 @@ class HashTableUnitTest {
         hashTable.put("BLOB", 3);
         int count = 0;
         for (HashTable.Entry<String, Number> entry : hashTable) {
-            assertNotNull(entry.getKey());
-            assertNotNull(entry.getValue());
+            Assertions.assertNotNull(entry.getKey());
+            Assertions.assertNotNull(entry.getValue());
             count++;
         }
-        assertEquals(3, count);
+        Assertions.assertEquals(3, count);
     }
 
     @Test
     void testCollisionHandling() {
         hashTable.put("Aa", 1);
         hashTable.put("BB", 2);
-        assertEquals(1, hashTable.get("Aa"));
-        assertEquals(2, hashTable.get("BB"));
+        Assertions.assertEquals(1, hashTable.get("Aa"));
+        Assertions.assertEquals(2, hashTable.get("BB"));
     }
 }
