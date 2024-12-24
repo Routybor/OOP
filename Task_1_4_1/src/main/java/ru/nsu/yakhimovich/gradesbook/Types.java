be76407fb5135c46253865eb0bfe25d572e99964
Daniel <routybor@gmail.com>
2024-12-24 15:13:38 +0700

    Task_1_4_1 (#9)
    * Impemented
    
    * Tets
    
    * Code style fixes
    
    * Code style fixes 2
    
    * Code fixes
    
    * Code style fixes 3

---
+++ Task_1_4_1/src/main/java/ru/nsu/yakhimovich/gradesbook/Types.java
@@ -0,0 +1,15 @@
+package ru.nsu.yakhimovich.gradesbook;
+
+/**
+ * Типы аттестации.
+ */
+public enum Types {
+    TASK,
+    TEST,
+    COLLOQ,
+    EXAM,
+    DIF_CREDIT,
+    CREDIT,
+    PRACTICE,
+    VKR
+}
